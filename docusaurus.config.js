6eecf1d6fcdcc6327f1bd3bb0599419523a643f1
Yehor Podporinov <yehor.podporinov@Yehors-MacBook-Pro.local>
2023-11-06 13:03:39 +0200

    added prism custom theme

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,8 +1,7 @@
 // @ts-check
 // Note: type annotations allow type checking and IDEs autocompletion
 
-const lightCodeTheme = require("prism-react-renderer/themes/github");
-const darkCodeTheme = require("prism-react-renderer/themes/dracula");
+const darkCodeTheme = require("./prism-custom-theme");
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
@@ -96,7 +95,6 @@ const config = {
         ],
       },
       prism: {
-        theme: lightCodeTheme,
         darkTheme: darkCodeTheme,
       },
     }),
